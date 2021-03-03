4c7ccb9a5ad5485241b72089c78c40a272c50b6a
FilloBoop <filippolandini03@gmail.com>
2021-03-03 12:44:59 +0100

    ho aggiunto un file

---
+++ c++.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+using namespace std;
+int main ()
+{
+   int n=3;
+   for(int i=0;i<3;i++)
+   {
+   	cout<<"giovanni merda"<<endl;
+   }
+   return 0;
+}
+
